de358084733a9c925d1f05c7d6233dad2304e19b
Carola <alvarezcarolina838@gmail.com>
2024-10-16 17:40:06 -0300

    Realizamos un programa en Python para convertitr cm a pulgadas para el trabajo final del curso de Python 1

---
+++ Conversion-medidas.py
@@ -0,0 +1,12 @@
+
+# Paso 1: Solicitar al usuario que ingrese la medida del diseño en cm
+
+Medida_en_cm = float(input("Por favor, ingrese la medida de la pieza en cm: "))
+
+#Paso 2: Realizar la conversión según el cálculo medida_en_pulgada = medida_en_cm/2.54
+
+Medida_en_pulgada = Medida_en_cm/2.54
+
+#Paso 3: Mostrar el resultado
+
+print("La medida en pulgadas de la piez es:", Medida_en_pulgada)
